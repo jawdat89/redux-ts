4500da82ab6c7e80c1a95b6a0b74b69b43434e4a
Jawdat Abadullah <Jawdat.89@outlook.com>
2023-02-18 22:45:53 +0200

    Adding an Action Type Enum

--- src/state/reducers/repositoriesReducer.ts
+++ src/state/reducers/repositoriesReducer.ts
@@ -4,29 +4,40 @@ interface RepositoriesState {
   data: string[];
 }
 
+
+type RepositoriesActions =
+  SearchRepositoriesActions |
+  SearchRepositoriesSuccessAction |
+  SearchRepositoriesErrorAction;
+
+enum RepositoriesActionTyps {
+  SEARCH_REPOSITORIES = 'search_repositories',
+  SEARCH_REPOSITORIES_SUCCESS = 'search_repositories_success',
+  SEARCH_REPOSITORIES_ERROR = 'search_repositories_error'
+}
+
 interface SearchRepositoriesActions {
-  type: 'search_repositories';
+  type: RepositoriesActionTyps.SEARCH_REPOSITORIES
 }
 
 interface SearchRepositoriesSuccessAction {
-  type: 'search_repositories_success';
+  type: RepositoriesActionTyps.SEARCH_REPOSITORIES_SUCCESS;
   payload: string[];
 }
 
 interface SearchRepositoriesErrorAction {
-  type: 'search_repositories_error';
+  type: RepositoriesActionTyps.SEARCH_REPOSITORIES_ERROR;
   payload: string;
 }
 
-declare type RepositoriesActions = SearchRepositoriesActions | SearchRepositoriesSuccessAction | SearchRepositoriesErrorAction;
 
 const reducer = (state: RepositoriesState, action: RepositoriesActions): RepositoriesState => {
   switch (action.type) {
-    case 'search_repositories':
+    case RepositoriesActionTyps.SEARCH_REPOSITORIES:
       return { loading: true, error: null, data: [] };
-    case 'search_repositories_success':
+    case RepositoriesActionTyps.SEARCH_REPOSITORIES_SUCCESS:
       return { loading: false, error: null, data: action.payload };
-    case 'search_repositories_error':
+    case RepositoriesActionTyps.SEARCH_REPOSITORIES_ERROR:
       return { loading: false, error: action.payload, data: [] };
     default:
       return state;
