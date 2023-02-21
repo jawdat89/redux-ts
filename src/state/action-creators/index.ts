aefe0eca8d441df575e03c435baa682d2bf4a01b
Jawdat Abadullah <Jawdat.89@outlook.com>
2023-02-21 21:52:02 +0200

    Adding Request Logic

--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -2,14 +2,27 @@ import axios from "axios";
 import { RepositoriesActionType } from "../action-types";
 import { RepositoriesAction } from '../actions';
 
-const searchRepositories = (term: string) => {
+export const searchRepositories = (term: string) => {
   return async (dispatch: any) => { 
     dispatch({
       type: RepositoriesActionType.SEARCH_REPOSITORIES
     });
 
     try {
-      
+      const { data } = await axios.get('https://registry.npmjs.org/-/v1/search', {
+        params: {
+          text: term
+        }
+      });
+
+      const names = data.objects.ma((result: any) => { 
+        return result.package.name;
+      });
+
+      dispatch({
+        type: RepositoriesActionType.SEARCH_REPOSITORIES_SUCCESS,
+        payload: names
+      });
     } catch (err) {
       if (err instanceof Error) {
         dispatch({
